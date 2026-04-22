89e0a09c64e21b7b93b567536c6762cb3d2564a4
Szymon Zborowski <szymon.zborowski@leancode.pl>
2026-04-22 22:17:09 +0200

    Built web
    Branch: main
    Commit: 0c92dd707b9aa31332f86f5eb2d7e2a391a3bec3
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1611205567"
+    serviceWorkerVersion: "1693876116"
   }
 });
